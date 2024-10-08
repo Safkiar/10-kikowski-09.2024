e9a1856b3482b3012aa50efe1b19286e0286660b
Safkiar <m.kikowski97@gmail.com>
2024-10-08 15:29:18 +0200

    spinner for img

--- src/components/Introduction/Introduction.jsx
+++ src/components/Introduction/Introduction.jsx
@@ -1,8 +1,9 @@
 import styled, {keyframes} from "styled-components";
 import JaImage from '../../assets/Ja.jpg'
 import { FaArrowDown } from "react-icons/fa";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { ScrollContext } from "../../utility/SmoothScroll";
+import Spinner from "../../ui/Spinner"
 
 const moveInRight = keyframes`
   0% {
@@ -172,31 +173,48 @@ const Quoter = styled.blockquote`
 
 
 const Introduction = () => {
+  const { scrollToNextSection } = useContext(ScrollContext);
+  const [isImageLoading, setIsImageLoading] = useState(true);
 
-    const { scrollToNextSection } = useContext(ScrollContext);
-
-    const handleScrollDown = () => {
-      scrollToNextSection();
-    };
-
- 
-  
-
-    return (<StyledDiv>
-
-         <Styledh1>Michał Kikowski</Styledh1>
-         <img src={JaImage} alt="Michał Kikowski"/>
-         <Styledh2>Fullstack Developer</Styledh2>
-         <Quoter>
-         <p> Dedicated and proactive developer, consistently improving skills through daily coding and hands-on experience, with a focus on problem-solving and efficient solutions </p>
-       
-
-     
-      </Quoter>
-      <Styledh4>Explore portfolio</Styledh4>
-      <button onClick={handleScrollDown}> <FaArrowDown /> </button>
-
-            </StyledDiv>);
+  const handleScrollDown = () => {
+    scrollToNextSection();
   };
-  
-  export default Introduction;
+
+  useEffect(() => {
+    const img = new Image();
+    img.src = JaImage;
+    img.onload = () => {
+      setIsImageLoading(false);
+    };
+  }, []); 
+  return (
+    <StyledDiv>
+      {isImageLoading ? (
+        <Spinner />
+      ) : (
+        <>
+          <Styledh1>Michał Kikowski</Styledh1>
+          <img
+            src={JaImage}
+            alt="Michał Kikowski"
+            style={{ display: isImageLoading ? "none" : "block" }}
+          />
+          <Styledh2>Fullstack Developer</Styledh2>
+          <Quoter>
+            <p>
+              Dedicated and proactive developer, consistently improving skills
+              through daily coding and hands-on experience, with a focus on
+              problem-solving and efficient solutions
+            </p>
+          </Quoter>
+          <Styledh4>Explore portfolio</Styledh4>
+          <button onClick={handleScrollDown}>
+            <FaArrowDown />
+          </button>
+        </>
+      )}
+    </StyledDiv>
+  );
+};
+
+export default Introduction;
